9f36d6b34475b830b65052a085b6a7967daaa305
Konstantin S. <havendv@gmail.com>
2026-09-15 22:36:27 +0400

    feat: Updated OpenAPI spec (#325)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/DeepInfra/Generated/DeepInfra.AccountClient.RequestRateLimitIncrease.g.cs
+++ src/libs/DeepInfra/Generated/DeepInfra.AccountClient.RequestRateLimitIncrease.g.cs
@@ -437,6 +437,43 @@ namespace DeepInfra
                                         h => h.Key,
                                         h => h.Value));
                             }
+                            // Too Many Requests
+                            if ((int)__response.StatusCode == 429)
+                            {
+                                string? __content_429 = null;
+                                global::System.Exception? __exception_429 = null;
+                                global::DeepInfra.DeepError? __value_429 = null;
+                                try
+                                {
+                                    if (__effectiveReadResponseAsString)
+                                    {
+                                        __content_429 = await __response.Content.ReadAsStringAsync(__effectiveCancellationToken).ConfigureAwait(false);
+                                        __value_429 = global::DeepInfra.DeepError.FromJson(__content_429, JsonSerializerContext);
+                                    }
+                                    else
+                                    {
+                                        __content_429 = await __response.Content.ReadAsStringAsync(__effectiveCancellationToken).ConfigureAwait(false);
+
+                                        __value_429 = global::DeepInfra.DeepError.FromJson(__content_429, JsonSerializerContext);
+                                    }
+                                }
+                                catch (global::System.Exception __ex)
+                                {
+                                    __exception_429 = __ex;
+                                }
+
+
+                                throw global::DeepInfra.ApiException<global::DeepInfra.DeepError>.Create(
+                                    statusCode: __response.StatusCode,
+                                    message: __content_429 ?? __response.ReasonPhrase ?? string.Empty,
+                                    innerException: __exception_429,
+                                    responseBody: __content_429,
+                                    responseObject: __value_429,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
+                                        __response.Headers,
+                                        h => h.Key,
+                                        h => h.Value));
+                            }
                             // Not Found
                             if ((int)__response.StatusCode == 404)
                             {
